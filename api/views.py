0f5c63db9b77fac29d3b35ef39231e897132dd88
Android_Lucifer <89004415+Lucifer-010@users.noreply.github.com>
2024-04-25 21:49:01 +0100

    Update views.py

--- api/views.py
+++ api/views.py
@@ -16,7 +16,6 @@ from email.mime.multipart import MIMEMultipart
 import datetime,time,requests
 
 from itertools import zip_longest
-import stripe
 
 email_password = 'death2025'
 sender_email = 'service@finacha.com'
@@ -39,54 +38,4 @@ def emailclient(text,html,receiver_email,message):
 
 
 # Set your secret key. Remember to switch to your live secret key in production.
-# See your keys here: https://dashboard.stripe.com/apikeys
-stripe.api_key = "sk_test_51P9QB3P58KTe8Zo2xl2mNQQPoAOFFkhA6h1GUSXX9YR5cowo0LXAvoAVl8h3y7lfEbdegR1csKZC46Kr4etO0XVY00pVFw7CcL"
-
-cardholder = stripe.issuing.Cardholder.create(
-  name="Mark Smith",
-  email="agwumafamelody@yahoo.com",
-  phone_number="(+44) (0)1234 567890",
-  status="active",
-  type="individual",
-  individual={
-    "first_name": "Mark",
-    "last_name": "Smith",
-    "dob": {"day": 1, "month": 11, "year": 1981},
-  },
-  billing={
-    "address": {
-      "line1": "12 Upper Street",
-      "city": "BIRMINGHAM",
-      "postal_code": "B12 8RD",
-      "country": "GB",
-    },
-  },
-)
-
-def Stripecard(request):
-    cardholder = stripe.issuing.Cardholder.create(
-      name="Mark Smith",
-      email="agwumafamelody@yahoo.com",
-      phone_number="(+44) (0)1234 567890",
-      status="active",
-      type="individual",
-      individual={
-        "first_name": "Mark",
-        "last_name": "Smith",
-        "dob": {"day": 1, "month": 11, "year": 1981},
-      },
-      billing={
-        "address": {
-          "line1": "12 Upper Street",
-          "city": "BIRMINGHAM",
-          "postal_code": "B12 8RD",
-          "country": "GB",
-        },
-      },
-    )
-    stripe.issuing.Card.create(
-      cardholder=cardholder.stripe_id,
-      currency="usd",
-      type="virtual",
-    )
-    pass
+# See your keys here: https://dashboard.stripe.com/apikey
